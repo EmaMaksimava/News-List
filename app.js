80c6745248af8215c91a52362ce635330f528fe0
Emma Maksimava <cielvibritania@gmail.com>
2021-09-05 00:39:06 +0300

    new project

---
+++ app.js
@@ -0,0 +1,190 @@
+// Custom Http Module
+function customHttp() {
+  return {
+    get(url, cb) {
+      try {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.addEventListener('load', () => {
+          if (Math.floor(xhr.status / 100) !== 2) {
+            cb(`Error. Status code: ${xhr.status}`, xhr);
+            return;
+          }
+          const response = JSON.parse(xhr.responseText);
+          cb(null, response);
+        });
+
+        xhr.addEventListener('error', () => {
+          cb(`Error. Status code: ${xhr.status}`, xhr);
+        });
+
+        xhr.send();
+      } catch (error) {
+        cb(error);
+      }
+    },
+    post(url, body, headers, cb) {
+      try {
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', url);
+        xhr.addEventListener('load', () => {
+          if (Math.floor(xhr.status / 100) !== 2) {
+            cb(`Error. Status code: ${xhr.status}`, xhr);
+            return;
+          }
+          const response = JSON.parse(xhr.responseText);
+          cb(null, response);
+        });
+
+        xhr.addEventListener('error', () => {
+          cb(`Error. Status code: ${xhr.status}`, xhr);
+        });
+
+        if (headers) {
+          Object.entries(headers).forEach(([key, value]) => {
+            xhr.setRequestHeader(key, value);
+          });
+        }
+
+        xhr.send(JSON.stringify(body));
+      } catch (error) {
+        cb(error);
+      }
+    },
+  };
+}
+// Init http module
+const http = customHttp();
+
+const newsService = (function() {
+  const apiKey = '092acbe0a560414d8f71c431e45e5d47';
+  const apiUrl = 'https://newsapi.org/v2';
+
+  return {
+    topHeadlines(country = 'ru', category = 'general', cb) {
+      http.get(`${apiUrl}/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}`, 
+      cb,
+      );
+    },
+    everything(query, cb) {
+      http.get(`${apiUrl}/everything?q=${query}&apiKey=${apiKey}`, 
+      cb,
+      );
+    }
+  }
+})();
+
+// Elements
+const form = document.forms['newsControls'];
+const countrySelect = form.elements['country'];
+const searchInput = form.elements['search'];
+const categoryInput = form.elements['category'];
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  loadNews();
+});
+
+//  init selects
+document.addEventListener('DOMContentLoaded', function() {
+  M.AutoInit();
+  loadNews();
+});
+
+// Load news function
+function loadNews() {
+  const country = countrySelect.value;
+  const searchText = searchInput.value;
+  const category = categoryInput.value;
+  showLoader();
+  if(!searchText) {
+    newsService.topHeadlines(country, category, onGetResponse);
+  } else {
+    newsService.everything(searchText, onGetResponse);
+  }
+
+  
+}
+
+// Function on get response from server
+function onGetResponse(err, res) {
+  removePreloader();
+  if (err) {
+    showAlert(err, 'error-msg');
+    return;
+  }
+  if (!res.articles.length) {
+    //showAlert('нет новостей с таким запросом');
+    return;
+  }
+  renderNews(res.articles);
+}
+
+// Function render news
+function renderNews(news) {
+  const newsContainer = document.querySelector('.news-container .row');
+  if (newsContainer.children.length) {
+    clearContainer(newsContainer);
+  }
+  let fragment = '';
+
+  news.forEach(newsItem => {
+    const el = newsTemplate(newsItem);
+    fragment += el;
+  });
+
+  newsContainer.insertAdjacentHTML('afterbegin', fragment);
+}
+
+//Function clear container
+function clearContainer(container) {
+  let child = container.lastElementChild;
+  while(child) {
+    container.removeChild(child);
+    child = container.lastElementChild;
+  }
+}
+
+//News item template function
+function newsTemplate({ urlToImage, title, url, description }) {
+  
+  const imgUrl = urlToImage ? urlToImage : 'http://risovach.ru/upload/2016/11/mem/internet-ubivaet_130010717_orig_.jpg';
+  return `
+    <div class="col s12">
+      <div class="card">
+        <div class="card-image">
+          <img src="${imgUrl}">
+          <span class="card-title">${title || ''}</span>
+        </div>
+        <div class="card-content">
+          <p>${description || ''} </p>
+        </div>
+        <div class="card-action">
+          <a href="${url}">Read more</a>
+        </div>
+      </div>
+    </div>
+  `;
+}
+
+function showAlert(msg, type = 'success') {
+  M.toast({ html: msg, classes: type});
+}
+
+// Show Preloader
+function showLoader() {
+  document.body.insertAdjacentHTML('afterbegin', 
+  `
+  <div class="progress">
+    <div class="indeterminate"></div>
+  </div>
+`)
+}
+
+// Remove loader
+function removePreloader() {
+  const loader = document.querySelector('.progress');
+  if (loader) {
+    loader.remove();
+  }
+}
